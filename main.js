f168bfbb48245329817f9ea3d92a8e668069ae9c
ctdupuis <ctdupuis7@gmail.com>
2020-04-03 18:37:06 -0500

    Done.

--- main.js
+++ main.js
@@ -4,9 +4,41 @@ const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
 
+let likeBtns = document.getElementsByClassName('like')
 
+for (const btn of likeBtns) {
+  btn.addEventListener("click", addLike)
+}
 
+function fillHeart(heart) {
+  heart.innerText = FULL_HEART;
+  heart.className = "activated-heart"
+}
 
+function clearHeart(heart) {
+  heart.innerText = EMPTY_HEART;
+  heart.className = "";
+}
+
+function addLike(e) {
+  let btn = e.target;
+  mimicServerCall("url")
+    .then(function(serverMsg) {
+      alert(serverMsg);
+      if (btn.innerText === EMPTY_HEART) {
+        fillHeart(btn);
+      } else {
+        clearHeart(btn);
+      }
+    })
+    .catch(function(message) {
+      debugger;
+      let errorMsg = document.getElementById("modal");
+      errorMsg.className = "";
+      errorMsg.innerHTML += `${message}`
+      setTimeout(function() { errorMsg.className = "hidden"}, 5000);
+    });
+}
 //------------------------------------------------------------------------------
 // Ignore after this point. Used only for demo purposes
 //------------------------------------------------------------------------------
