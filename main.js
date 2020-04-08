c4c4efa671c60571a6fe9db9f40722c2f30e0c7d
kalmegati <kalmegati@gmail.com>
2020-04-08 15:40:41 -0400

    Done.

--- main.js
+++ main.js
@@ -4,14 +4,31 @@ const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
 
+hearts = document.getElementsByClassName("like-glyph");
 
+for (const heart of hearts) {
+  heart.addEventListener("click", mimicServerCall.bind(heart), false)
+};
 
+function heartify(heat) {
+  const wuv = heat.target;
+  if (wuv.classList.length < 2) {
+    wuv.textContent = '♥';
+    wuv.classList.add("activated-heart");
+    console.log("owo");
+  }
+  else {
+    wuv.textContent = '♡'
+    wuv.classList.remove("activated-heart");
+    console.log("uwu");
+  };
+};
 
 //------------------------------------------------------------------------------
 // Ignore after this point. Used only for demo purposes
 //------------------------------------------------------------------------------
 
-function mimicServerCall(url="http://mimicServer.example.com", config={}) {
+function mimicServerCall(heat, url="http://mimicServer.example.com", config={}) {
   return new Promise(function(resolve, reject) {
     setTimeout(function() {
       let isRandomFailure = Math.random() < .2
@@ -19,6 +36,7 @@ function mimicServerCall(url="http://mimicServer.example.com", config={}) {
         reject("Random server error. Try again.");
       } else {
         resolve("Pretend remote server notified of action!");
+        heartify(heat);
       }
     }, 300);
   });
