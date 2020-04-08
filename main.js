fe818612fb301e48771bb99c5147218a5a898c18
jaredjames2020 <jaredjames2020@gmail.com>
2020-04-08 17:14:21 -0400

    removes error message, fills and empty's heart by button click

--- main.js
+++ main.js
@@ -3,7 +3,40 @@ const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
+let likeHeartButtons = document.getElementsByClassName('like')
 
+for (const button of likeHeartButtons) {
+  button.addEventListener("click", likeHeart)
+}
+
+function fullHeart(heart) {
+  heart.innerText = FULL_HEART;
+  heart.className = "activated-heart"
+}
+
+function emptyHeart(heart) {
+  heart.innerText = EMPTY_HEART;
+  heart.className = "";
+}
+
+function likeHeart(e) {
+  let button = e.target;
+  mimicServerCall("url")
+    .then(function(serverCallMessage) {
+      alert(serverCallMessage);
+      if (button.innerText === EMPTY_HEART) {
+        fullHeart(button);
+      } else {
+        emptyHeart(button);
+      }
+    })
+    .catch(function(error) {
+      let errorMessage = document.getElementById("modal");
+      errorMessage.className = "";
+      errorMessage.innerHTML += `${error}`
+      setTimeout(function() { errorMessage.className = "hidden"}, 5000);
+    });
+}
 
 
 
