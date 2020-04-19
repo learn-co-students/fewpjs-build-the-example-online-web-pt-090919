cebde76d54b81d5cfec936bdb2dea4c2099ab47c
fasnimo <fasnimo@gmail.com>
2020-04-19 12:14:49 -0500

    Done.

--- main.js
+++ main.js
@@ -3,8 +3,37 @@ const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
+let heartBtn = document.querySelector('.like')
+let modal = document.querySelector('#modal')
+let modalMessage = modal.querySelector('#modal-message')
 
+heartBtn.forEach(btn => {
+  btn.addEventListener('click', (event) => {
+    let glyph = btn.querySelector('span.like-glyph')
+    if (glyph.textContent === EMPTY_HEART) {
+      glyph.textContent = FULL_HEART
+      glyph.classList.add('activated-heart')
+    } else {
+      glyph.textContent = EMPTY_HEART
+      glyph.classList.remove('activated-heart')
+    }
+  })
+})
 
+mimicServerCall()
+.then(serverMessage => {
+  alert('Server Alerted')
+  alert(serverMessage)
+})
+.catch(error => {
+  modal.classList.remove('hidden');
+  modelMessage.textContent = error.message
+  setTimeout(hideModal, 5000)
+  })
+
+  const hideModal = () => {
+    modal.classList.add('hidden')
+  }
 
 
 //------------------------------------------------------------------------------
